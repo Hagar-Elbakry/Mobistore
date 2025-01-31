1ddee58403a837cefe4e83a3f16f8808c20be345
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-01-31 15:35:57 +0200

    Add conditional rendering for cart and wishlist views with not found templates

--- app/views/Cart.view.php
+++ app/views/Cart.view.php
@@ -3,9 +3,8 @@
     ob_start();
     
     include 'Header.view.php';
-
-    include "Templates/_cart.view.php";
-    include "Templates/_wishlist.view.php";
+    count($product->getData('cart')) ? include "Templates/_cart.view.php" : include "Templates/_cart-notfound.view.php";
+    count($product->getData('wishlist')) ? include "Templates/_wishlist.view.php" : include "Templates/_wishlist-notfound.view.php";
     include "Templates/_new-phones.view.php";
 
     include "Footer.view.php";
